aae0d525c4ea879872aa432c7b8c773f4b6ee903
Kyle Florence <kyle.florence@joinmosaic.com>
2014-12-06 23:24:18 -0800

    Use external helpers where possible

--- source/helpers/defining.js
+++ source/helpers/defining.js
@@ -1,22 +1,2 @@
-module.exports.register = function(Handlebars) {
-  /**
-   * Define `@data` properties inside of a block.
-   *
-   * @param {Object} options.hash A hash of properties to set.
-   *
-   * @example {{#defining foo="bar"}}@foo{{/defining}}
-   */
-  Handlebars.registerHelper("defining", function(options) {
-    var data;
-
-    if (options.data && options.hash) {
-      data = Handlebars.createFrame(options.data);
-      Object.keys(options.hash).forEach(function(key) {
-        data[key] = options.hash[key];
-      });
-      options.data = data;
-    }
-
-    return options.fn(this, options);
-  });
-};
+var helpers = require("mosaic-handlebars-helpers");
+module.exports.register = helpers.defining.register;
